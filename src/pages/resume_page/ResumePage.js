1c5c72a54ba11de3f1fde37dcc343a7afccc3eef
Engineer <chaolongpiao@gmail.com>
2024-04-26 20:54:26 -0700

    Update ResumePage.js

--- src/pages/resume_page/ResumePage.js
+++ src/pages/resume_page/ResumePage.js
@@ -3,17 +3,17 @@ import Button from "react-bootstrap/Button";
 import { AiOutlineDownload } from "react-icons/ai";
 import Particle from "../../Particle";
 import pdf from "../../assets/My-resume.pdf";
-import { pdfjs } from "react-pdf";
+import {  Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import Zoom from "react-reveal/Zoom";
 import React, { useState, useEffect } from "react";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-// const resumeLink =
-//   "https://raw.githubusercontent.com/MD-MAFUJUL-HASAN/Personal-Portfolio/main/src/assets/React-CV.pdf";
+const resumeLink =
+  "https://github.com/daniel-gig/My-portfolio/blob/main/src/assets/My-resume.pdf";
 
 function Resume() {
-  const [setWidth] = useState(1200);
+  const [width,setWidth] = useState(1200);
 
   useEffect(() => {
     setWidth(window.innerWidth);
@@ -42,6 +42,17 @@ function Resume() {
                 </Button> 
               </div>
             </div>
+            <div className="resume d-flex justify-content-center">
+                <Document file={resumeLink}>
+                  <Page pageNumber={1} scale={width > 786 ? 1.6 : 0.4} />
+                </Document>
+              </div>
+              <div className="d-flex justify-content-center">
+                <Button variant="primary" href={pdf} target="_blank">
+                  <AiOutlineDownload />
+                  &nbsp;Download Resume
+                </Button>
+              </div>
           </Container>
         </Container>
       </section>
